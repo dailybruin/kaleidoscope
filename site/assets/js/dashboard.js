4d8c6c869a4dac0d851feb99729684cf0fe3e6b9
schung507 <schung507@gmail.com>
2016-11-08 21:20:52 -0800

    got inputs to hide and show

--- site/assets/js/dashboard.js
+++ site/assets/js/dashboard.js
@@ -1,3 +1,79 @@
 $(document).ready(function() {
-	alert("JS loaded under site/assets/js/dashboard.js");
+	
+	$(".author").hide();
+	$(".image").hide();
+	$(".title").hide();
+	$(".quote").hide();
+	$(".text").hide();
+	$(".cover").hide();
+
+
+	$('#attribute').change(function () {
+
+		var dropdown = document.getElementById("attribute");
+		var value = dropdown.options[dropdown.selectedIndex].text;
+
+		switch ( value){			
+
+			case "author":
+				$(".author").show();
+				$(".image").hide();
+				$(".title").hide();
+				$(".quote").hide();
+				$(".text").hide();
+				$(".cover").hide();
+				break;
+			case "image":
+				
+				$(".image").show();
+				$(".author").hide();
+				$(".title").hide();
+				$(".quote").hide();
+				$(".text").hide();
+				$(".cover").hide();
+				break;
+			case "title":
+				
+				$(".title").show();
+				$(".author").hide();
+				$(".image").hide();
+				$(".quote").hide();
+				$(".text").hide();
+				$(".cover").hide();
+				break;
+			case "quote":
+				
+				$(".quote").show();
+				$(".author").hide();
+				$(".image").hide();
+				$(".title").hide();
+				$(".text").hide();
+				$(".cover").hide();
+				break;
+			case "text section":
+				
+				$(".text").show();
+				$(".author").hide();
+				$(".image").hide();
+				$(".title").hide();
+				$(".quote").hide();
+				$(".cover").hide();
+				break;
+			case "cover image":
+				
+				$(".cover").show();
+				$(".author").hide();
+				$(".image").hide();
+				$(".title").hide();
+				$(".quote").hide();
+				$(".text").hide();
+				break;
+			default:
+				$(".form").hide();
+		}
+
+	});
+	
+
+
 });
