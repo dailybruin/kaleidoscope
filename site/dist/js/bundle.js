a693e9fddfd9be7d1da72c5c42fc19eebbfe49c5
shannonphu <shannonphu@sbcglobal.net>
2016-11-10 18:40:03 -0800

    start porting dashboard to react.js

--- site/dist/js/bundle.js
+++ site/dist/js/bundle.js
@@ -96,13 +96,13 @@
 
 	var _react2 = _interopRequireDefault(_react);
 
-	var _commonUser = __webpack_require__(4);
-
-	var _commonUser2 = _interopRequireDefault(_commonUser);
-
-	var _commonPage = __webpack_require__(5);
+	var _commonPage = __webpack_require__(4);
 
 	var _commonPage2 = _interopRequireDefault(_commonPage);
+
+	var _Dashboard = __webpack_require__(5);
+
+	var _Dashboard2 = _interopRequireDefault(_Dashboard);
 
 	// Creates a App components that works as the base of the app.
 
@@ -126,7 +126,9 @@
 	                    null,
 	                    'App'
 	                ),
-	                _react2['default'].createElement(_commonPage2['default'], { title: 'Article 1', author: 'John Smith' })
+	                _react2['default'].createElement(_commonPage2['default'], { title: 'Article 1', author: 'John Smith' }),
+	                _react2['default'].createElement('hr', null),
+	                _react2['default'].createElement(_Dashboard2['default'], { componentTypes: ['image', 'title', 'cover image', 'author', 'quote', 'text section'] })
 	            );
 	        }
 	    }]);
@@ -141,77 +143,6 @@
 
 /***/ },
 /* 4 */
-/***/ function(module, exports, __webpack_require__) {
-
-	"use strict";
-
-	Object.defineProperty(exports, "__esModule", {
-	    value: true
-	});
-
-	var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
-
-	var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
-
-	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-	var _react = __webpack_require__(2);
-
-	var _react2 = _interopRequireDefault(_react);
-
-	// Super basic component that takes name and age and then prints it
-
-	var User = (function (_React$Component) {
-	    _inherits(User, _React$Component);
-
-	    _createClass(User, null, [{
-	        key: "propTypes",
-	        value: {
-	            name: _react2["default"].PropTypes.string.isRequired,
-	            age: _react2["default"].PropTypes.number.isRequired
-	        },
-	        enumerable: true
-	    }]);
-
-	    function User(props) {
-	        _classCallCheck(this, User);
-
-	        _get(Object.getPrototypeOf(User.prototype), "constructor", this).call(this, props);
-	    }
-
-	    _createClass(User, [{
-	        key: "render",
-	        value: function render() {
-	            return _react2["default"].createElement(
-	                "div",
-	                { className: "User" },
-	                _react2["default"].createElement(
-	                    "p",
-	                    null,
-	                    "Name: ",
-	                    this.props.name,
-	                    _react2["default"].createElement("br", null),
-	                    "Age: ",
-	                    this.props.age
-	                )
-	            );
-	        }
-	    }]);
-
-	    return User;
-	})(_react2["default"].Component);
-
-	;
-
-	exports["default"] = User;
-	module.exports = exports["default"];
-
-/***/ },
-/* 5 */
 /***/ function(module, exports, __webpack_require__) {
 
 	"use strict";
@@ -279,6 +210,98 @@
 	;
 
 	exports["default"] = Page;
+	module.exports = exports["default"];
+
+/***/ },
+/* 5 */
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+
+	Object.defineProperty(exports, "__esModule", {
+	    value: true
+	});
+
+	var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+	var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+	var _react = __webpack_require__(2);
+
+	var _react2 = _interopRequireDefault(_react);
+
+	var Dashboard = (function (_React$Component) {
+	    _inherits(Dashboard, _React$Component);
+
+	    _createClass(Dashboard, null, [{
+	        key: "propTypes",
+	        value: {
+	            componentTypes: _react2["default"].PropTypes.array.isRequired
+	        },
+	        enumerable: true
+	    }]);
+
+	    function Dashboard(props) {
+	        _classCallCheck(this, Dashboard);
+
+	        _get(Object.getPrototypeOf(Dashboard.prototype), "constructor", this).call(this, props);
+	        this.state = { selectedComponent: this.props.componentTypes[0] };
+	        this.handleDropdownChange = this.handleDropdownChange.bind(this);
+	        this.handleSubmit = this.handleSubmit.bind(this);
+	    }
+
+	    _createClass(Dashboard, [{
+	        key: "render",
+	        value: function render() {
+	            var components = this.props.componentTypes.map(function (type, i) {
+	                return _react2["default"].createElement(
+	                    "option",
+	                    { value: type.replace(/\s/g, "_") },
+	                    type
+	                );
+	            });
+
+	            return _react2["default"].createElement(
+	                "div",
+	                { className: "Dashboard" },
+	                _react2["default"].createElement(
+	                    "form",
+	                    { onSubmit: this.handleSubmit },
+	                    _react2["default"].createElement(
+	                        "select",
+	                        { value: this.state.selectedComponent, onChange: this.handleDropdownChange },
+	                        components
+	                    ),
+	                    _react2["default"].createElement("input", { type: "submit" })
+	                )
+	            );
+	        }
+	    }, {
+	        key: "handleDropdownChange",
+	        value: function handleDropdownChange(event) {
+	            this.setState({ selectedComponent: event.target.value });
+	            console.log('Dropdown changed: ' + event.target.value);
+	        }
+	    }, {
+	        key: "handleSubmit",
+	        value: function handleSubmit(event) {
+	            console.log('A name was submitted: ' + this.state.selectedComponent);
+	            event.preventDefault();
+	        }
+	    }]);
+
+	    return Dashboard;
+	})(_react2["default"].Component);
+
+	;
+
+	exports["default"] = Dashboard;
 	module.exports = exports["default"];
 
 /***/ }
