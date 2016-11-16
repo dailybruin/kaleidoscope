e43f842775ea268897d8cd9ef019029bd88038e0
jasonxjiang@github.com <jiang.jason.x@gmail.com>
2016-11-15 19:17:35 -0800

    adding redux stores to dashboard

--- site/jsx/components/Dashboard.jsx
+++ site/jsx/components/Dashboard.jsx
@@ -1,4 +1,63 @@
 import React from 'react';
+import {createStore, combineReducers }from 'redux';
+
+// currently keep track of one thing which is componnent rendering 
+
+
+const dashboard_reducer = function(state = [], action) {
+    console.log("intialized an empty array for state");
+    switch (action.type) {
+        case 'ADD_NEW_COMPONENT':
+            return {
+                ...state,
+                message: action.value
+            }
+        default:
+            return state;
+    }
+}
+
+// const page_reducer = function(state = [], action) {
+//     switch (action.type){
+//         default:
+//             return state;
+//     }
+// }
+
+// const reducer = combineReducers({
+//     dashboard: dashboard_reducer,
+//     page: page_reducer,
+// })
+
+// let store_0 = createStore(reducer);
+
+// store_0.dispatch({
+//     type: 'ADD_NEW_COMPONENT',
+// })
+
+// store_0.dispatch({
+//     type: 'ADD_IMAGE',
+// })
+
+// const addImage = function (src, credit, caption) {
+//     return {
+//         type: 'ADD_IMAGE',
+//         src: src,
+//         credit: credit,
+//         caption: caption,
+//     }
+// }
+
+// const addNewComponent = function (component) {
+//     return{
+//         type: 'ADD_NEW_COMPONENT',
+//         component: component
+//     }
+// }
+// const test_url = 'https://s-media-cache-ak0.pinimg.com/originals/14/37/10/143710e981aedc43f8091f066c645660.jpg';
+// const test_credit = 'TEH INTERNET';
+// const test_caption = 'Can I haz cupcake?';
+// console.log(store_0.dispatch(addImage(test_url,test_credit,test_caption)));
 
 class Dashboard extends React.Component {
     static propTypes = {
@@ -7,6 +66,7 @@ class Dashboard extends React.Component {
     constructor(props) {
         super(props);
         this.state = {selectedComponent: this.props.componentTypes[0]};
+        // add syntatic sugar () => {} to prevent exessive bind calls 
         this.handleDropdownChange = this.handleDropdownChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
