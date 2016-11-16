ce28b92c871503359fee765e891bfcfcfabbdb57
Jerry Liu <jerrylzy@ucla.edu>
2016-11-15 20:42:20 -0800

    Refactor Dashboard State fields

--- site/jsx/components/Dashboard.jsx
+++ site/jsx/components/Dashboard.jsx
@@ -1,25 +1,23 @@
 import React from 'react';
 
-var Contents = 
-{
-    type = "",
-    author = "",
-    title = "",
-    subheading = "",
-    text = "",
-    quote = "",
-    quoteMaker = "",
-    image = "",
-    imageCaption = ""
-};
-
 class Dashboard extends React.Component {
     static propTypes = {
         componentTypes: React.PropTypes.array.isRequired
     }
     constructor(props) {
         super(props);
-        this.state = {selectedComponent: this.props.componentTypes[0], contents: Contents};
+        this.state = {selectedComponent: this.props.componentTypes[0], 
+            type: '', 
+            author: '', 
+            title: '',
+            subheading: '',
+            text: '',
+            quote: '',
+            quoteMaker: '',
+            imageUrl: '',
+            imageCredit: '',
+            imageCaption: ''
+        };
         this.handleDropdownChange = this.handleDropdownChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
@@ -50,17 +48,19 @@ class Dashboard extends React.Component {
     handleDropdownChange(event) {
         this.setState({selectedComponent: event.target.value});
         // {this.showInputForComponentType(event.target.value)}
-        this.setState({contents.type: event.target.value})
+        this.setState({type: event.target.value})
     }
 
     handleSubmit(event) {
         console.log('A name was submitted: ' + this.state.selectedComponent);
         event.preventDefault();
         
+        var contents = 
+
         $.ajax({
           url: '/store',
           dataType: 'json',
-          data: contentsMap;
+          // data: this.state.contents;
           type: 'POST'
         });
     }
@@ -70,28 +70,28 @@ class Dashboard extends React.Component {
         switch(componentType) {
             case 'title':
                 return(
-                    <div><input placeholder="Title" type="text" name="title" className="form-control"/></div>
+                    <div><input placeholder="Title" type="text" name="title" value={this.state.title} className="form-control"/></div>
                 );
                 break;
             case 'author':
                 return(
-                    <div><input placeholder="Author" type="text" name="author" required="required" className="form-control"/></div>
+                    <div><input placeholder="Author" type="text" name="author" required="required" value={this.state.author} className="form-control"/></div>
                 );
                 break;
             case 'image':
                 return(
                     <div>
-                        <input placeholder="URL" type="text" name="url" className="form-control"/>
-                        <input placeholder="Credit" type="text" name="credit" className="form-control"/>
-                        <input placeholder="Caption" type="text" name="caption" className="form-control"/>
+                        <input placeholder="URL" type="text" name="url" value={this.state.imageUrl} className="form-control"/>
+                        <input placeholder="Credit" type="text" name="credit" value={this.state.imageCredit} className="form-control"/>
+                        <input placeholder="Caption" type="text" name="caption" value={this.state.imageCaption} className="form-control"/>
                     </div>
                 );
                 break;
             case 'quote':
                 return(
                     <div>
-                        <input placeholder="Quote" type="text" name="quote" className="form-control"/>
-                        <input placeholder="Quote Maker" type="text" name="quoteMaker" className="form-control"/>
+                        <input placeholder="Quote" type="text" name="quote" value={this.state.quote} className="form-control"/>
+                        <input placeholder="Quote Maker" type="text" name="quoteMaker" value={this.state.quoteMaker} className="form-control"/>
                     </div>
                 );
                 break;
@@ -105,21 +105,35 @@ class Dashboard extends React.Component {
                 break;
         }
     }
-
-    clearContents()
+    selectFields(type)
     {
-        Contents = 
-        {
-            type = "",
-            author = "",
-            title = "",
-            subheading = "",
-            text = "",
-            quote = "",
-            quoteMaker = "",
-            image = "",
-            imageCaption = ""
-        };
+        var contents = {};
+        switch(type) {
+            case 'title':
+                contents['title'] = this.state.title;
+                break;
+            case 'author':
+                contents['author'] = this.state.author;
+                break;
+            case 'image':
+                contents['url'] = this.state.imageUrl;
+                contents['caption'] = this.state.imageCaption;
+                contents['credit'] = this.state.imageCredit;
+                break;
+            case 'quote':
+                contents['quote'] = this.state.quote;
+                contents['quoteMaker'] = this.state.quoteMaker;
+                break;
+            case 'text_section':
+                return(
+                    <div><textarea name="text" cols="90" rows="8"></textarea></div>
+                );
+                break;
+            default:
+                return(<p>nothing</p>);
+                break;
+        }
+        return contents;
     }
 };
 
