175bf445eb53a0ee88a2249b5ccf372a24030f10
jasonxjiang <jiang.jason.x@gmail.com>
2017-02-10 18:18:25 -0800

    drag stuff

--- site/jsx/components/common/DashboardItem.js
+++ site/jsx/components/common/DashboardItem.js
@@ -2,7 +2,28 @@ import React from 'react';
 
 export default class DashboardItem extends React.Component {
 	static propTypes = {
-		component: React.PropTypes.element,
-		metadata: 
+		component: React.PropTypes.element.isRequired,
+		type: React.PropTypes.string.isRequired,
+		database_id: React.PropTypes.string.isRequired,
+		button: React.PropTypes.element.isRequired,
+	}
+
+	constructor(props){
+		super(props);
+		this.state = {
+			value: 0,
+		}
+	}
+
+	render() {
+		const {item, itemSelected, dragHandle} = this.props;
+		return (
+				<div>
+					{this.props.component}
+					{this.props.button}
+				</div>
+			)
+		}
+
 	}
 }
