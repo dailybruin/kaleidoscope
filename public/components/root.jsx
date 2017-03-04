791fe0b336b12f5785ded1c1a330a36c27ccffb2
shannonphu <shannonphu@sbcglobal.net>
2017-03-03 18:04:49 -0800

    cleaned and commented root.jsx

--- public/components/root.jsx
+++ public/components/root.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './App'
 import createStore from '../redux/reducers/index'
-import { Provider } from 'react-redux'
-import { connect } from 'react-redux';
-import ReactDOM from 'react-dom';
+import { Provider, connect } from 'react-redux'
 import SortableComponent from './common/SortableComponent'
-// import {render} from 'react-dom'
 
 const store = createStore();
 
 var mountNode = document.getElementById("react-main-mount");
 
+// Set in the URL from /all if updating a page
 var components = JSON.parse($('#app meta').attr('components'));
 var database_id = JSON.parse($('#app meta').attr('database_id'));
-
 
 ReactDOM.render(
 	<Provider store={store}>
