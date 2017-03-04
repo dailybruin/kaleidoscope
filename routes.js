ae0e8ff9f87259bc2585d1070cdd46c6c0e2b2ad
shannonphu <shannonphu@sbcglobal.net>
2017-03-03 17:58:11 -0800

    commented routes.js

--- routes.js
+++ routes.js
@@ -1,19 +1,25 @@
-/**
- * Routes
- * Catches all GET/POST requests
- */
-var express = require('express');
+// Routes
+//    Catches all GET/POST requests
+
 var Page = require('./public/model/page');
 var mongodb = require('mongodb');
-
 var GenPage = null;
 
 module.exports = function (app) {
+    // Renders index
     app.get('/', function (req, res) {
         res.render('index', { title: 'New Page', components: [], database_id: "" });
     });
 
-    /* GET saved pages */
+    // Load specific pre-existing page obtained from /all into /
+    app.get('/update', function(req, res, next) {
+        Page.findOne({'_id': req.query.pageID}, function(err, page) {
+            // console.log(page.components);
+            res.render('index', { title: 'Editting Page', components: page.components, database_id: req.query.pageID } );
+        });
+    });
+
+    // Show all previously stored pages
     app.get('/all', function(req, res) {
         var pages;
         Page.find(function (err, pages) {
@@ -22,14 +28,9 @@ module.exports = function (app) {
         });
     });
 
-    /* GET create page form. */
-    app.get('/update', function(req, res, next) {
-        Page.findOne({'_id': req.query.pageID}, function(err, page) {
-            // console.log(page.components);
-            res.render('index', { title: 'Editting Page', components: page.components, database_id: req.query.pageID } );
-        });
-    });
-
+    // Save a new or update a currently existing page
+    //    - new page made if no 'current_id' passed in
+    //    - otherwise updating the page with the id passed down
     app.post('/gen', function(req, res, next) {
         if (GenPage == null) {
             GenPage = new Page();
@@ -68,6 +69,7 @@ module.exports = function (app) {
         }
     });
 
+    // Deleting a page from the database selected in /all
     app.post('/page/:id', function (req, res, next) {
         console.log('deleting ' + req.params.id);
         Page.remove({_id: new mongodb.ObjectID(req.params.id)}, function(err, results) {
